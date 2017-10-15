8c8c93d781420c207ad8b2fa3f711c3ee0929969
Gogs <gogs@fake.local>
2017-10-15 23:10:46 +0100

    stickler test

---
+++ reduce.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { pick } = require('underscore')
+
+
+const c = {
+  log: 'this',
+  error: 'that',
+  warn: 'those',
+  info: 'them'
+}
+
+// gotta be a better way...
+const cm = ['log', 'info', 'warn', 'error']
+const pc = pick(c, cm)
+cm.reduce((n, v) => { n[v] = () => {}; return n }, c)
+Object.assign(c, pc)
